69d0ae004070392329c71962ec70527018870401
Colin Gordon <cogo@umich.edu>
2026-02-28 06:57:09 -0800

    progress on warp tile copy from ref

--- gemms.cu
+++ gemms.cu
@@ -70,10 +70,64 @@ __device__ void loadFromGmem(int N, int K, const float *A, const float *B,
   }
 }
 
-__device__ void writeToGmem(int N, int K, int innerRowA, int innerColA, int innerRowB, int innerColB, float* C) {
-  // do we write with the float4 thing as well?
+template <const int BM, const int BN, const int BK, const int WM, const int WN,
+          const int WMITER, const int WNITER, const int WSUBM, const int WSUBN,
+          const int TM, const int TN>
+__device__ void processFromSmem(float *regM, float *regN, float *threadResults, const float *As,
+                const float *Bs, const uint warpRow, const uint warpCol,
+                const uint threadRowInWarp, const uint threadColInWarp) {
+  // here is where we read into As, Bs, and do the warp tiling itself
+  
+  // outer loop - need to loop over all k in BK
+  for(int dotIdx = 0; dotIdx < BK; ++dotIdx) {
+
+    //1. read into regM
+    // WMITER is basically the number of times we need the warp on axis M to perform its action
+    for(int wSubtileRowIdx = 0; wSubtileRowIdx < WMITER; ++wSubtileRowIdx) {
+      for(int i = 0; i < WSUBM; ++i) {
+        /* this is a lot so let me break it down: 
+        As is BM * BK, and stored transposed. This means BM rows, BK cols.
+        what is in As? The tile that the current block is!
+        so we have a whole tile in As. Thus, first we must index along the BK dimension,
+        as this is 
+        dotidx * BM = we're at the start of the tile and getting to the warp that we care about 
+        warpRow * WM = we're at the start of the warp we care about getting to the row in the warp where our thread is
+        wSubtileRowIdx * WSUBM = we're at the col (bc transpose) in warp where our thread is, and want to TODO: i need to think / read more on this
+        */
+        regM[wSubtileRowIdx * TM + i] = As[(dotIdx * BM) + warpRow * WM + wSubtileRowIdx * WSUBM + threadRowInWarp * TM + i];
+    }
+  }
+
+  //2. read into regN
+  // WSUBN = WN/WNITER = 16 (used to iterate over slices)
+  for(int wSubtileColIdx = 0; wSubtileColIdx < WNITER; ++wSubtileColIdx) {
+    for(int i = 0; i < WSUBN; ++i) {
+
+      regN[wSubtileColIdx * TN + i] = Bs[(dotIdx * BN) + warCol * WN + wSubtileColIdx * WSUBN + threadColInWarp * TN + i];
+    }
+  }
+
+
+
+  // from here, pretending the index calcs + data loading is all done - 
+  // time to do the actual warp-tile!
+
+  for(int wSubtileRowIdx = 0; wSubtileRowIdx < WMITER; ++wSubtileRowIdx) {
+    for(int wSubtileColIdx = 0; wSubtileColIdx < WNITER; ++wSubtileColIdx) {
+      for(int resIdxM = 0; resIdxM < TM; ++resIdxM) {
+        for(int resIdxN = 0; resIdxN < TN; ++resIdxN) {
+          // TODO: annotate the indexing here
+          threadResults[(wSubtileRowIdx * TM + resIdxM) * (WNITER * TN) + (wSubtileColIdx * TN) + resIdxN] = 
+          regM[(wSubtileRowIdx * TM) + resIdxM] * regN[(wSubtileColIdx * TN) + resIdxN];
+        }
+      } 
+    }
+
+  }
+
   
 }
+                }
 
 } // namespace ws
 
@@ -96,52 +150,80 @@ __global__ void __launch_bounds__(NUM_THREADS)
 
   // the overall goal of this section is to get the indexing of our data correct
   // row/col within the output tile in C
-  int cRow = threadIdx.x;
-  int cCol = threadIdx.y;
-
-  // TO DEFINE:
-  int rowStrideA;
-  int rowStrideB;
-  int innerRowA;
-  int innerColA;
-  int innerRowB;
-  int innerColB;
 
 
 
-  // 1. get warp indexing
+  // from the top: block to warp to thread indexing
+
+  //tile size == block size? NO! actually idk TODO: figure out the discrepancy that i am missing here
+  // run through the basics: 1 block can have 4 warps active = 128 threads at a time.int
+  // consider 1024 x 1024 matrix.
+  // split into 128 x 128 tiles, 8 x 8 grid of tiles
+  // each SM gets 1 tile because there are more sms than tiles
+  // TODO: think about above as well
+  int blockIdxM = blockIdx.x;
+  int blockIdxN = blockIdx.y;
+
+  int numWarpsN = BN / WN;
+  int warpIdx = threadIdx.x / WARPSIZE;
+  int warpCol = warpIdx % numWarpsN;
+  int warpRow = warpIdx / numWarpsN;
+
+  //TODO: grasp what this even means and annotate it
+  constexpr uint WMITER = (WM * WN) / (WARPSIZE * TM * TN * WNITER);
+  constexpr uint WSUBM = WM / WMITER; // 64/2=32
+  constexpr uint WSUBN = WN / WNITER; // 32/2=16
   
-
-  // 2. get warp subtile size
-
-
-  // get thread placement within the subtile
+  int numThreadsPerSubtile = WSUBN / TN;
+  int threadIdxInWarp = threadIdx.x % warpIdx;
+  int threadColInWarp = threadIdxInWarp % numThreadsPerSubtile;
+  int threadRowInWarp = threadIdxInWarp / numThreadsPerSubtile;
 
 
   // shared mem
   __shared__ float As[BM * BK];
   __shared__ float Bs[BN * BK];
 
-  // load the data into the smem with our function
-  wt::loadFromGmem<BM, BN, BK, rowStrideA, rowStrideB>(N, N, A, B, As, Bs, innerRowA, innerColA, innerRowB, innerColB);
-  // using the smem tiles, use our indices + a short loop on TM, TN to do our acc
-  float acc[TM][TN];
+  //set A, B to correct offset
+  A += blockIdxM * BM * K;
+  B += blockIdxN * BN * K;
 
-  for(int k=0; k < BK; ++k) {
-    //load fragment of As
-    float* A_frag;
-    //load fragment of Bs
-    float* B_frag;
-    // do outer prod into acc
-    for(int m_sub = 0; m_sub < TM; ++m_sub) {
-      for (int n_sub = 0; n_sub < TN; ++n_sub) {
-        acc[m_sub][n_sub] = A_frag[m_sub] * B_frag[n_sub];
-    }
-  }
+  // set C to warp output tile
+  C += (blockIdxM * BM + warpRow * WM) + blockIdxN * BN + warpCol * WN;
+
+  // set all the inner stuff + strides:
+  // remember 4 float vectorized loads
+  int ARowSizeInf4 = BK / 4;
+  int BRowSizeInf4 = BN / 4;
+  int rowStrideA = NUM_THREADS  / ARowSizeInf4;
+  int rowStrideB = NUM_THREADS / BRowSizeInf4;
+  int innerRowA = threadIdx.x / ARowSizeInf4;
+  int innerColA = threadIdx.x % ARowSizeInf4;
+  int innerRowB = threadIdx.x / BRowSizeInf4;
+  int innerColB = threadIdx.x % BRowSizeInf4;
+
+  // using the smem tiles, use our indices + a short loop on TM, TN to do our acc
+  float threadResults[TM * WMITER * TN * WNITER] = {0.0}
+
+  float regM[WMITER * TM];
+  float regN[WNITER * TN];
+
+  for(int blockKIdx=0; blockKIdx < K; blockKIdx += BK) {
+    //populate As, Bs
+    wt::loadFromGmem<BM, BN, BK, rowStrideA, rowStrideB>(N, K, A, B, As, Bs, innerRowA, innerColA, innerRowB, innerColB);
+    __syncthreads();
+    // go from As, Bs to registers, do the warp tiling and write it back to C
+    wt::processFromSmem<BM, BN, BK, WM, WN, WMITER, WNITER, WSUBM, WSUBN, TM, TN>
+    (regM, regN, threadResults, As, Bs, warpRow, warpCol, threadRowInWarp, threadColInWarp);
+    // move A and B to next block
+    A += BK;
+    B += BK * N;
+    __syncthreads(); 
   }
   
-  // use a function to write from the acc to C
-
+  // write from the acc to C
+  // more for loops!
+  
   // Done!
 }
 
